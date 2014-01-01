f5be46ff891a2f93af1f4981a77bba55bce1892e
Yuan <yuan@yuan-VirtualBox.(none)>
2014-01-02 00:27:00 +0800

    thread

--- processPrint2.c
+++ processPrint2.c
@@ -1,22 +1,56 @@
-#include  <stdio.h>
-#include  <string.h>
-#include  <sys/types.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-#define   MAX_COUNT  20
-#define   BUF_SIZE   100
+#define MAXCOUNT 20
 
-int  main(void)
+int sharedCounter = 0;
+
+
+pthread_mutex_t lock= PTHREAD_MUTEX_INITIALIZER;
+
+void * thread_odd()
 {
-     pid_t  pid;
-     int    i;
-     char   buf[BUF_SIZE];
+        while(sharedCounter<MAXCOUNT){
+		sleep(1);
+		pthread_mutex_lock(&lock);
+		printf("Odd acquired lock-");
+		if(sharedCounter%2 != 0) {                
+			printf("Odd print: %d ", sharedCounter);
+			sharedCounter++;
+		}
+                pthread_mutex_unlock(&lock);
+		printf("-Odd released lock \n");
+		
+        }
+}
 
-     fork();
-     pid = getpid();
-     for (i = 1; i <= MAX_COUNT; i++) {
-          sprintf(buf, "This line is from pid %d, value = %d\n", pid, i);
-          write(1, buf, strlen(buf));
-     } 
-     
-     return 1;
+void * thread_even()
+{
+        while(sharedCounter<MAXCOUNT){
+		sleep(1);
+		pthread_mutex_lock(&lock);
+		printf("Even acquired lock-");	
+		if(sharedCounter%2 == 0) {                
+			printf("Odd print: %d ", sharedCounter);
+			sharedCounter++;
+		}
+                pthread_mutex_unlock(&lock);
+		printf("-Even released lock \n");
+
+        }
+}
+
+int main()
+{
+        int status;
+        pthread_t tid1,tid2;
+
+        pthread_create(&tid1,NULL,&thread_odd, NULL);
+        pthread_create(&tid2,NULL,&thread_even,NULL);
+        pthread_join(tid1,NULL);
+        pthread_join(tid2,NULL);
+        
+	return 0;
 }
